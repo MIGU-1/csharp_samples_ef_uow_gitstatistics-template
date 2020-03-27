37a53a1507b8c0818142cac4d36cca1d47b93cff
Michael Gutenbrunner <m.gutenbrunner@itag.at>
2020-03-27 10:22:07 +0100

    implements blocks

--- source/GitStat.ImportConsole/ImportController.cs
+++ source/GitStat.ImportConsole/ImportController.cs
@@ -11,14 +11,79 @@ namespace GitStat.ImportConsole
     public class ImportController
     {
         const string Filename = "commits.txt";
+        private static List<Developer> _devops = new List<Developer>();
 
         /// <summary>
         /// Liefert die Messwerte mit den dazugehörigen Sensoren
         /// </summary>
         public static Commit[] ReadFromCsv()
         {
-            throw new NotImplementedException();
+            List<Commit> commits = new List<Commit>();
+            string[] lines = System.IO.File.ReadAllLines(MyFile.GetFullNameInApplicationTree(Filename));
+            List<CommitBlock> blocks = GetBlocks(lines);
+            _devops = GetDevops(blocks);
         }
 
+        private static List<Developer> GetDevops(List<CommitBlock> blocks)
+        {
+            foreach (CommitBlock block in blocks)
+            {
+                string[] headData = block.Head.Split(',');
+                string hashCode = headData[0];
+                string Name = headData[1];
+                DateTime date = DateTime.Parse(headData[2]);
+                string message = headData[3];
+
+                if (_devops.Where(d => d.Name == Name).SingleOrDefault() == null)
+                {
+                    Developer devop = new Developer
+                    {
+                        Name = Name,
+                    };
+
+                    _devops.Add(devop);
+                }
+            }
+        }
+
+        private static List<CommitBlock> GetBlocks(string[] lines)
+        {
+            List<CommitBlock> blocks = new List<CommitBlock>();
+            int idx = 0;
+
+            while (idx < lines.Length)
+            {
+                CommitBlock block = new CommitBlock();
+
+                while (idx < lines.Length && !String.IsNullOrEmpty(lines[idx]))
+                {
+                    if (block.Head != null)
+                    {
+                        block.BodyLines.Add(lines[idx]);
+                    }
+                    else
+                    {
+                        block.Head = lines[idx];
+                    }
+
+                    idx++;
+                }
+
+                blocks.Add(block);
+                idx++;
+            }
+
+            return blocks;
+        }
+        private class CommitBlock
+        {
+            public string Head { get; set; }
+            public List<string> BodyLines { get; set; }
+
+            public CommitBlock()
+            {
+                BodyLines = new List<string>();
+            }
+        }
     }
 }
--- source/GitStat.Persistence/Migrations/20190320063615_InitialMigration.cs
+++ source/GitStat.Persistence/Migrations/20190320063615_InitialMigration.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using GitStat.Persistence;
 
 namespace GitStat.Persistence.Migrations
 {
